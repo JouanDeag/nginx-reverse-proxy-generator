8787f76436746c87d486bae3e1c740eff8a21b0d
JouanDeag <61161558+JouanDeag@users.noreply.github.com>
2021-12-18 17:47:42 +0100

    Buttons, Codeblock and some styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,5 @@
-const list = document.querySelector('.list');
-const addBtn = document.querySelector(".addBtn");
-const clearBtn = document.querySelector('.clearBtn');
-const noteInput = document.querySelector('.note');
+const genbtn = document.getElementsByClassName("gen-btn");
+let output = document.getElementsByClassName("finalconfig")
 
-function deleteNote(e) {
-  const parent = e.target.parentNode;
-  parent.remove();
-}
-
-function addStuff() {
-  
-  const note = noteInput.value;
-  
-  if(note.length === 0) return;
-    
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.textContent = 'Delete note';
-  btn.addEventListener("click", (e) => deleteNote(e));
-  
-  const li = document.createElement('li');
-  li.textContent = `${note} `;
-  li.appendChild(btn);
-  
-  list.appendChild(li);
-  
-  noteInput.value = "";
-}
-
-function clearNotes() {
-  list.innerHTML  = ''
-}
-
-addBtn.addEventListener("click", addStuff);
-clearBtn.addEventListener("click", clearNotes);
-
-                          
-
+console.log(output);
+console.log(genbtn);
